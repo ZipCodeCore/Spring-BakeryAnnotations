1e7606363d751afed3574f87bd725ba409ae41dd
Jeremy <jeremysabina@gmail.com>
2021-08-17 10:12:13 -0400

    Do the hustlegit add .

--- src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
+++ src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
@@ -2,33 +2,46 @@ package com.zipcodewilmington.bakery.controllers;
 
 import com.zipcodewilmington.bakery.models.Baker;
 import com.zipcodewilmington.bakery.services.BakerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
+@RequestMapping(value = "/baker-controller")
 public class BakerController {
+    @Autowired
     private BakerService service;
 
     public BakerController(BakerService service) {
         this.service = service;
     }
 
+    @RequestMapping(method = RequestMethod.GET, value = "/index")
     public ResponseEntity<Iterable<Baker>> index() {
         return new ResponseEntity<>(service.index(), HttpStatus.OK);
     }
 
+    @RequestMapping(method = RequestMethod.GET, value = "/show")
     public ResponseEntity<Baker> show(Long id) {
         return new ResponseEntity<>(service.show(id), HttpStatus.OK);
     }
 
-    public ResponseEntity<Baker> create(Baker baker) {
+    @RequestMapping(method = RequestMethod.POST, value = "/create")
+    public ResponseEntity<Baker> create(@RequestBody Baker baker) {
         return new ResponseEntity<>(service.create(baker), HttpStatus.CREATED);
     }
-
-    public ResponseEntity<Baker> update(Long id, Baker baker) {
+    @RequestMapping(method = RequestMethod.POST, value = "/update")
+    public ResponseEntity<Baker> update(@PathVariable Long id,@RequestBody Baker baker) {
         return new ResponseEntity<>(service.update(id, baker), HttpStatus.OK);
     }
 
-    public ResponseEntity<Boolean> destroy(Long id) {
-        return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
+    @RequestMapping(method = RequestMethod.DELETE, value = "/destroy")
+    public ResponseEntity<Boolean> destroy(@PathVariable Long id) {
+        return new ResponseEntity<>(service.delete(id), HttpStatus.NO_CONTENT);
     }
 }
